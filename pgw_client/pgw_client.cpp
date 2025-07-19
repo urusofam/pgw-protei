5659564060c2cf37acf40921c0f030aac9b7eba7
urusofam <urusofam@gmail.com>
2025-07-19 23:27:41 +0800

    Добавил перевод IMSI из командной строки в BCD в клиенте

--- pgw_client/pgw_client.cpp
+++ pgw_client/pgw_client.cpp
@@ -4,12 +4,26 @@
 #include <exception>
 #include <iostream>
 
-int main() {
+#include "bcd.h"
+
+int main(int argc, char* argv[]) {
     try {
+        if (argc != 2) {
+            std::cerr << "Использование: pgw_client <imsi>" << '\n';
+            return 1;
+        }
+
         client_config config = load_client_config("configs/client.json");
         setup_logger(config.log_file, config.log_level);
         spdlog::info("Конфиг и логгер загружен");
+
+        std::string IMSI = argv[1];
+        std::vector<uint8_t> BCD = imsi_to_bcd(IMSI);
+        spdlog::info("Imsi получен и успешно перекодирован в BCD");
     } catch (std::exception& e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << e.what() << '\n';
+        return 1;
     }
+
+    return 0;
 }
