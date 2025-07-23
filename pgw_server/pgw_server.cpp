236db1ecb5cf0923df2e278c930cd6cf10c1a443
urusofam <urusofam@gmail.com>
2025-07-23 12:33:28 +0800

    Убрал лишнюю строку для дебага

--- pgw_server/pgw_server.cpp
+++ pgw_server/pgw_server.cpp
@@ -6,7 +6,6 @@
 
 int main() {
     try {
-        std::cout << "Current path: " << std::filesystem::current_path() << std::endl;
         server_config config = load_server_config("configs/server.json");
         setup_logger(config.log_file, config.log_level);
         spdlog::info("Конфиг и логгер загружен");
