37f4e8991699bb23592e7fd3a3efe7cc65a8fdd0
urusofam <urusofam@gmail.com>
2025-07-24 11:30:43 +0800

    Упростил тесты для общей бибилитоеки

--- tests/common_lib_test.cpp
+++ tests/common_lib_test.cpp
@@ -5,45 +5,37 @@
 #include "logger.h"
 #include <spdlog/spdlog.h>
 
-#include "spdlog/sinks/stdout_sinks-inl.h"
-
 // Тесты bcd
-class bcd_test : public ::testing::Test {
-protected:
-    static void SetUpTestSuite() {
-        spdlog::set_level(spdlog::level::off);
-    }
-};
 
 // Пустой imsi
-TEST_F(bcd_test, empty_imsi) {
+TEST(bcd_test, empty_imsi) {
     ASSERT_THROW(imsi_to_bcd(""), std::invalid_argument);
 }
 
 // Валидная ситуация
-TEST_F(bcd_test, correct_situation) {
-    std::vector<uint8_t> bcd = imsi_to_bcd("346051602396626");
-    std::string imsi = bcd_to_imsi(bcd);
+TEST(bcd_test, correct_situation) {
+    const std::vector<uint8_t> bcd = imsi_to_bcd("346051602396626");
+    const std::string imsi = bcd_to_imsi(bcd);
     ASSERT_EQ(imsi, "346051602396626");
 }
 
 // Не только числа в imsi
-TEST_F(bcd_test, not_a_number_in_imsi) {
+TEST(bcd_test, not_a_number_in_imsi) {
     ASSERT_THROW(imsi_to_bcd("34605160239662x");, std::invalid_argument);
 }
 
 // Тесты настройки логгера
 class logger_test : public ::testing::Test {
 protected:
-    void SetUp() override {
-        spdlog::drop_all();
-        spdlog::set_default_logger(nullptr);
+    std::string log_filename = "test_log.txt";
+
+    void TearDown() override {
+        std::filesystem::remove_all("logs");
     }
 };
 
 // Валидная ситуация
 TEST_F(logger_test, correct_situation) {
-    std::string log_filename = "test_log.log";
     ASSERT_NO_THROW(setup_logger(log_filename, "info"));
 
     // Проверяем, что логгер был установлен
@@ -69,8 +61,8 @@ TEST_F(logger_test, correct_situation) {
 
 // Двойной вызов настройки логгера
 TEST_F(logger_test, twice_setup_logger_call) {
-    ASSERT_NO_THROW(setup_logger("test1.log", "info"));
-    ASSERT_NO_THROW(setup_logger("test2.log", "error"));
+    ASSERT_NO_THROW(setup_logger(log_filename, "info"));
+    ASSERT_NO_THROW(setup_logger(log_filename, "error"));
 
     // Проверяем, что последний вызов определяет настройки
     EXPECT_EQ(spdlog::get_level(), spdlog::level::err);
@@ -89,5 +81,6 @@ TEST_F(logger_test, invalid_log_level) {
 
 int main() {
     testing::InitGoogleTest();
+    spdlog::set_level(spdlog::level::off);
     return RUN_ALL_TESTS();
 }
