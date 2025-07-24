1b591f2e81640080a541b0b31f27c8972d6a8029
urusofam <urusofam@gmail.com>
2025-07-24 22:46:54 +0800

    Добавил отправление UDP пакета в клиентской части

--- pgw_client/pgw_client.cpp
+++ pgw_client/pgw_client.cpp
@@ -3,13 +3,16 @@
 #include "spdlog/spdlog.h"
 #include <exception>
 #include <iostream>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
 #include "bcd.h"
 
 int main(int argc, char* argv[]) {
     try {
         if (argc != 2) {
-            std::cerr << "Использование: pgw_client <imsi>" << '\n';
+            std::cerr << "Использование: pgw_client <IMSI>" << '\n';
             return 1;
         }
 
@@ -19,7 +22,57 @@ int main(int argc, char* argv[]) {
 
         std::string IMSI = argv[1];
         std::vector<uint8_t> BCD = imsi_to_bcd(IMSI);
-        spdlog::info("Imsi получен и успешно перекодирован в BCD");
+        spdlog::info("IMSI получен и успешно перекодирован в BCD");
+
+        spdlog::info("Подготовка к отправке UDP пакета IMSI {} на сервер {}:{}", IMSI,
+            config.server_ip, config.server_port);
+
+        int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+        if (sockfd < 0) {
+            spdlog::critical("Не удалось создать UDP сокет: {}", strerror(errno));
+            return 1;
+        }
+
+        timeval tv{};
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+        setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+
+        sockaddr_in server_addr{};
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(config.server_port);
+        if (inet_pton(AF_INET, config.server_ip.c_str(), &server_addr.sin_addr) <= 0) {
+            spdlog::critical("Неправильный IP адрес {}", config.server_ip);
+            close(sockfd);
+            return 1;
+        }
+
+        if (sendto(sockfd, BCD.data(), BCD.size(), 0, (sockaddr*) &server_addr, sizeof(server_addr)) < 0) {
+            spdlog::critical("Не удалось отправить UDP пакет: {}", strerror(errno));
+            close(sockfd);
+            return 1;
+        }
+        spdlog::info("UDP пакет отправлен");
+
+        char buffer[config.udp_buffer_size];
+        ssize_t n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0, nullptr, nullptr);
+
+        if (n < 0) {
+            spdlog::critical("Не удалось получить ответ от сервера: {}", strerror(errno));
+            close(sockfd);
+            return 1;
+        }
+
+        if (n == sizeof(buffer) - 1) {
+            spdlog::warn("Возможно, ответ был обрезан (получено максимум байт)");
+        }
+
+        buffer[n] = '\0';
+        std::string response(buffer);
+        spdlog::info("Получен ответ от сервера: {}", response);
+        std::cout << response << '\n';
+
+        close(sockfd);
     } catch (std::exception& e) {
         std::cerr << e.what() << '\n';
         return 1;
